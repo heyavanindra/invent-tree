1ae407bd506c064d630acc0bf0909a62a5ca3b9b
Avanindra <heyavanindra@gmail.com>
2025-10-01 20:52:57 +0530

    fixed mobile responsiveness

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <nav className="">
       
         <Link href={"/"}>
-        <Image className="fixed top-0 z-30 left-0 max-sm:h-[100px] max-sm:w-[150px] " src={"/logo/Logo-with-background.png"} alt="Logo" height={200} width={200}></Image>
+        <Image className="fixed top-0 z-30 left-0 max-sm:w-[150px] max-sm:h-[150px]  " src={"/logo/Logo-with-background.png"} alt="Logo" height={200} width={200}></Image>
         </Link>
         <div className="fixed  right-7 top-7 sm:top-7 sm:right-7 flex items-center sm:h-[40px] z-60 sm:mx-10">
           {/* <MenuButton
